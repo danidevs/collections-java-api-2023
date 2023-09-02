b46d2fd5933455ca5a9918ca167503309b3e82f0
unknown <danielsk4te@hotmail.com>
2023-09-02 20:39:06 -0300

    Resolução Desafio list

---
+++ src/list/OperacaoBasica/ListaTarefa.java
@@ -0,0 +1,66 @@
+package list.OperacaoBasica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        if (!tarefaList.isEmpty()) {
+            for (Tarefa t : tarefaList) {
+                if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                    tarefasParaRemover.add(t);
+                }
+            }
+            tarefaList.removeAll(tarefasParaRemover);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        if (!tarefaList.isEmpty()) {
+            System.out.println(tarefaList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("tarefa 3");
+
+        System.out.println("Você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
+
+        listaTarefa.obterDescricoesTarefas();
+
+        listaTarefa.removerTarefa("Tarefa 1");
+
+        System.out.println("Agora você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
+
+        listaTarefa.obterDescricoesTarefas();
+
+        listaTarefa.removerTarefa("tarefa 3");
+
+        System.out.println("Agora você tem " + listaTarefa.obterNumeroTotalTarefas() + " tarefas na lista:");
+    }
+}
