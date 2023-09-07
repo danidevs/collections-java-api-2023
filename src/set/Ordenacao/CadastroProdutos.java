3c986fd1ef89376757cbac3a007e1cab574310c8
unknown <danielsk4te@hotmail.com>
2023-09-06 22:14:40 -0300

    Exercício ordenação

---
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,51 @@
+package set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        if (!produtoSet.isEmpty()) {
+            return produtosPorNome;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        if (!produtoSet.isEmpty()) {
+            produtosPorPreco.addAll(produtoSet);
+            return produtosPorPreco;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1L, "Parafuso", 1500d, 10000);
+        cadastroProdutos.adicionarProduto(2L, "Porca", 1500d, 5000);
+        cadastroProdutos.adicionarProduto(1L, "Prego", 30d, 20000);
+        cadastroProdutos.adicionarProduto(4L, "martelo", 50d, 150);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+}
---
+++ src/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,76 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+
+    private Set<Contato> contatosSet;
+
+    public AgendaContatos() {
+        this.contatosSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatosSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        if (!contatosSet.isEmpty()) {
+            System.out.println(contatosSet);
+        } else {
+            System.out.println("Lista de Contatos!");
+        }
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        if (!contatosSet.isEmpty()) {
+            for (Contato c : contatosSet) {
+                if (c.getNome().startsWith(nome)) {
+                    contatosPorNome.add(c);
+                }
+            }
+            return contatosPorNome;
+        } else {
+            throw new RuntimeException("Conatatos!");
+        }
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        if (!contatosSet.isEmpty()) {
+            for (Contato c : contatosSet) {
+                if (c.getNome().equalsIgnoreCase(nome)) {
+                    c.setNumero(novoNumero);
+                    contatoAtualizado = c;
+                    break;
+                }
+            }
+            return contatoAtualizado;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("João", 123456789);
+        agendaContatos.adicionarContato("Maria", 987654321);
+        agendaContatos.adicionarContato("Maria Silva", 55555555);
+        agendaContatos.adicionarContato("Ana", 88889999);
+        agendaContatos.adicionarContato("Camila", 77778888);
+        agendaContatos.adicionarContato("Carolina", 55555555);
+
+        agendaContatos.exibirContatos();
+
+        Contato contatoAtualizado = agendaContatos.atualizarNumeroContato("Camila", 44443333);
+        System.out.println("Contato atualizado: " + contatoAtualizado);
+
+        agendaContatos.exibirContatos();
+    }
+}
